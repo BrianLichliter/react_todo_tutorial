309c3b1b040bfa8d41fe563998e18ef0105c54c5
Brian Lichliter <brian.lichliter@gmail.com>
2017-08-19 18:14:07 -0500

    Debugging heroku pg connection

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ var Todo = require('./model/todos');
 //import dependency 
 var knex = require('knex')({
   client: 'pg',
-  connection: {
-    host : process.env.HOST || '127.0.0.1',
-    user : process.env.USER || 'bal',
-    password : process.env.PASSWORD || '',
-    database : process.env.DATABASE || 'bal'
+  connection: process.env.DATABASE_URL || {
+    host : '127.0.0.1',
+    user : 'bal',
+    password : '',
+    database : 'bal'
   }
 });
 
